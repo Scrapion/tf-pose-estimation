ae18098337d90acce95f4572da038bf6ee4b540f
Scrapion <40768074+Scrapion@users.noreply.github.com>
2020-11-12 15:19:26 +0100

    Add files via upload

--- tf_pose/estimator.py
+++ tf_pose/estimator.py
@@ -1,8 +1,8 @@
 import logging
-import math
 
 import slidingwindow as sw
 
+from tf_pose.pafprocess import pafprocess
 import cv2
 import numpy as np
 import tensorflow as tf
@@ -12,47 +12,28 @@ import time
 from tf_pose import common
 from tf_pose.common import CocoPart
 from tf_pose.tensblur.smoother import Smoother
-#from tensorflow.python.compiler.tensorrt import trt_convert as trt
 
-#try:
-from tf_pose.pafprocess import pafprocess
-"""
-except ModuleNotFoundError as e:
-    print(e)
-    print('you need to build c++ library for pafprocess. See : https://github.com/ildoonet/tf-pose-estimation/tree/master/tf_pose/pafprocess')
-    exit(-1)
-"""
 logger = logging.getLogger('TfPoseEstimator')
-logger.handlers.clear()
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
 formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
-logger.setLevel(logging.INFO)
-
-
-def _round(v):
-    return int(round(v))
-
-
-def _include_part(part_list, part_idx):
-    for part in part_list:
-        if part_idx == part.part_idx:
-            return True, part
-    return False, None
 
 
 class Human:
     """
     body_parts: list of BodyPart
     """
-    __slots__ = ('body_parts', 'pairs', 'uidx_list', 'score')
+    __slots__ = ('body_parts', 'pairs', 'uidx_list', 'score','x_parts','y_parts','scores')
 
     def __init__(self, pairs):
         self.pairs = []
         self.uidx_list = set()
         self.body_parts = {}
+        self.x_parts= []
+        self.y_parts = []
+        self.scores = []
         for pair in pairs:
             self.add_pair(pair)
         self.score = 0.0
@@ -84,158 +65,6 @@ class Human:
 
     def get_max_score(self):
         return max([x.score for _, x in self.body_parts.items()])
-
-    def get_face_box(self, img_w, img_h, mode=0):
-        """
-        Get Face box compared to img size (w, h)
-        :param img_w:
-        :param img_h:
-        :param mode:
-        :return:
-        """
-        # SEE : https://github.com/ildoonet/tf-pose-estimation/blob/master/tf_pose/common.py#L13
-        _NOSE = CocoPart.Nose.value
-        _NECK = CocoPart.Neck.value
-        _REye = CocoPart.REye.value
-        _LEye = CocoPart.LEye.value
-        _REar = CocoPart.REar.value
-        _LEar = CocoPart.LEar.value
-
-        _THRESHOLD_PART_CONFIDENCE = 0.2
-        parts = [part for idx, part in self.body_parts.items() if part.score > _THRESHOLD_PART_CONFIDENCE]
-
-        is_nose, part_nose = _include_part(parts, _NOSE)
-        if not is_nose:
-            return None
-
-        size = 0
-        is_neck, part_neck = _include_part(parts, _NECK)
-        if is_neck:
-            size = max(size, img_h * (part_neck.y - part_nose.y) * 0.8)
-
-        is_reye, part_reye = _include_part(parts, _REye)
-        is_leye, part_leye = _include_part(parts, _LEye)
-        if is_reye and is_leye:
-            size = max(size, img_w * (part_reye.x - part_leye.x) * 2.0)
-            size = max(size,
-                       img_w * math.sqrt((part_reye.x - part_leye.x) ** 2 + (part_reye.y - part_leye.y) ** 2) * 2.0)
-
-        if mode == 1:
-            if not is_reye and not is_leye:
-                return None
-
-        is_rear, part_rear = _include_part(parts, _REar)
-        is_lear, part_lear = _include_part(parts, _LEar)
-        if is_rear and is_lear:
-            size = max(size, img_w * (part_rear.x - part_lear.x) * 1.6)
-
-        if size <= 0:
-            return None
-
-        if not is_reye and is_leye:
-            x = part_nose.x * img_w - (size // 3 * 2)
-        elif is_reye and not is_leye:
-            x = part_nose.x * img_w - (size // 3)
-        else:  # is_reye and is_leye:
-            x = part_nose.x * img_w - size // 2
-
-        x2 = x + size
-        if mode == 0:
-            y = part_nose.y * img_h - size // 3
-        else:
-            y = part_nose.y * img_h - _round(size / 2 * 1.2)
-        y2 = y + size
-
-        # fit into the image frame
-        x = max(0, x)
-        y = max(0, y)
-        x2 = min(img_w - x, x2 - x) + x
-        y2 = min(img_h - y, y2 - y) + y
-
-        if _round(x2 - x) == 0.0 or _round(y2 - y) == 0.0:
-            return None
-        if mode == 0:
-            return {"x": _round((x + x2) / 2),
-                    "y": _round((y + y2) / 2),
-                    "w": _round(x2 - x),
-                    "h": _round(y2 - y)}
-        else:
-            return {"x": _round(x),
-                    "y": _round(y),
-                    "w": _round(x2 - x),
-                    "h": _round(y2 - y)}
-
-    def get_upper_body_box(self, img_w, img_h):
-        """
-        Get Upper body box compared to img size (w, h)
-        :param img_w:
-        :param img_h:
-        :return:
-        """
-
-        if not (img_w > 0 and img_h > 0):
-            raise Exception("img size should be positive")
-
-        _NOSE = CocoPart.Nose.value
-        _NECK = CocoPart.Neck.value
-        _RSHOULDER = CocoPart.RShoulder.value
-        _LSHOULDER = CocoPart.LShoulder.value
-        _THRESHOLD_PART_CONFIDENCE = 0.3
-        parts = [part for idx, part in self.body_parts.items() if part.score > _THRESHOLD_PART_CONFIDENCE]
-        part_coords = [(img_w * part.x, img_h * part.y) for part in parts if
-                       part.part_idx in [0, 1, 2, 5, 8, 11, 14, 15, 16, 17]]
-
-        if len(part_coords) < 5:
-            return None
-
-        # Initial Bounding Box
-        x = min([part[0] for part in part_coords])
-        y = min([part[1] for part in part_coords])
-        x2 = max([part[0] for part in part_coords])
-        y2 = max([part[1] for part in part_coords])
-
-        # # ------ Adjust heuristically +
-        # if face points are detcted, adjust y value
-
-        is_nose, part_nose = _include_part(parts, _NOSE)
-        is_neck, part_neck = _include_part(parts, _NECK)
-        torso_height = 0
-        if is_nose and is_neck:
-            y -= (part_neck.y * img_h - y) * 0.8
-            torso_height = max(0, (part_neck.y - part_nose.y) * img_h * 2.5)
-        #
-        # # by using shoulder position, adjust width
-        is_rshoulder, part_rshoulder = _include_part(parts, _RSHOULDER)
-        is_lshoulder, part_lshoulder = _include_part(parts, _LSHOULDER)
-        if is_rshoulder and is_lshoulder:
-            half_w = x2 - x
-            dx = half_w * 0.15
-            x -= dx
-            x2 += dx
-        elif is_neck:
-            if is_lshoulder and not is_rshoulder:
-                half_w = abs(part_lshoulder.x - part_neck.x) * img_w * 1.15
-                x = min(part_neck.x * img_w - half_w, x)
-                x2 = max(part_neck.x * img_w + half_w, x2)
-            elif not is_lshoulder and is_rshoulder:
-                half_w = abs(part_rshoulder.x - part_neck.x) * img_w * 1.15
-                x = min(part_neck.x * img_w - half_w, x)
-                x2 = max(part_neck.x * img_w + half_w, x2)
-
-        # ------ Adjust heuristically -
-
-        # fit into the image frame
-        x = max(0, x)
-        y = max(0, y)
-        x2 = min(img_w - x, x2 - x) + x
-        y2 = min(img_h - y, y2 - y) + y
-
-        if _round(x2 - x) == 0.0 or _round(y2 - y) == 0.0:
-            return None
-        return {"x": _round((x + x2) / 2),
-                "y": _round((y + y2) / 2),
-                "w": _round(x2 - x),
-                "h": _round(y2 - y)}
 
     def __str__(self):
         return ' '.join([str(x) for x in self.body_parts.values()])
@@ -284,9 +113,19 @@ class PoseEstimator:
             for part_idx in range(18):
                 c_idx = int(pafprocess.get_part_cid(human_id, part_idx))
                 if c_idx < 0:
+                    human.x_parts.append(0)
+                    human.y_parts.append(0)
+                    human.scores.append(0)
+                    humans.append(human)
                     continue
 
                 is_added = True
+               
+                #RICCARDO //TODO 
+                human.x_parts.append(float(pafprocess.get_part_x(c_idx)))
+                human.y_parts.append(float(pafprocess.get_part_y(c_idx)))
+                human.scores.append(pafprocess.get_part_score(c_idx))
+
                 human.body_parts[part_idx] = BodyPart(
                     '%d-%d' % (human_id, part_idx), part_idx,
                     float(pafprocess.get_part_x(c_idx)) / heat_mat.shape[1],
@@ -305,7 +144,7 @@ class PoseEstimator:
 class TfPoseEstimator:
     # TODO : multi-scale
 
-    def __init__(self, graph_path, target_size=(320, 240), tf_config=None, trt_bool=False):
+    def __init__(self, graph_path, target_size=(320, 240), tf_config=None):
         self.target_size = target_size
 
         # load graph
@@ -314,41 +153,25 @@ class TfPoseEstimator:
             graph_def = tf.compat.v1.GraphDef()
             graph_def.ParseFromString(f.read())
 
-        if trt_bool is True:
-            output_nodes = ["Openpose/concat_stage7"]
-            graph_def = trt.create_inference_graph(
-                graph_def,
-                output_nodes,
-                max_batch_size=1,
-                max_workspace_size_bytes=1 << 20,
-                precision_mode="FP16",
-                # precision_mode="INT8",
-                minimum_segment_size=3,
-                is_dynamic_op=True,
-                maximum_cached_engines=int(1e3),
-                use_calibration=True,
-            )
-
         self.graph = tf.compat.v1.get_default_graph()
         tf.import_graph_def(graph_def, name='TfPoseEstimator')
         self.persistent_sess = tf.compat.v1.Session(graph=self.graph, config=tf_config)
 
-        for ts in [n.name for n in tf.compat.v1.get_default_graph().as_graph_def().node]:
-            print(ts)
+        # for op in self.graph.get_operations():
+        #     print(op.name)
+        # for ts in [n.name for n in tf.get_default_graph().as_graph_def().node]:
+        #     print(ts)
 
         self.tensor_image = self.graph.get_tensor_by_name('TfPoseEstimator/image:0')
         self.tensor_output = self.graph.get_tensor_by_name('TfPoseEstimator/Openpose/concat_stage7:0')
         self.tensor_heatMat = self.tensor_output[:, :, :, :19]
         self.tensor_pafMat = self.tensor_output[:, :, :, 19:]
         self.upsample_size = tf.compat.v1.placeholder(dtype=tf.int32, shape=(2,), name='upsample_size')
-        self.tensor_heatMat_up = tf.compat.v1.image.resize(self.tensor_output[:, :, :, :19], self.upsample_size,
+        self.tensor_heatMat_up = tf.compat.v1.image.resize_area(self.tensor_output[:, :, :, :19], self.upsample_size,
                                                       align_corners=False, name='upsample_heatmat')
-        self.tensor_pafMat_up = tf.compat.v1.image.resize(self.tensor_output[:, :, :, 19:], self.upsample_size,
+        self.tensor_pafMat_up = tf.compat.v1.image.resize_area(self.tensor_output[:, :, :, 19:], self.upsample_size,
                                                      align_corners=False, name='upsample_pafmat')
-        if trt_bool is True:
-            smoother = Smoother({'data': self.tensor_heatMat_up}, 25, 3.0, 19)
-        else:
-            smoother = Smoother({'data': self.tensor_heatMat_up}, 25, 3.0)
+        smoother = Smoother({'data': self.tensor_heatMat_up}, 25, 3.0)
         gaussian_heatMat = smoother.get_output()
 
         max_pooled_in_tensor = tf.nn.pool(gaussian_heatMat, window_shape=(3, 3), pooling_type='MAX', padding='SAME')
@@ -386,17 +209,9 @@ class TfPoseEstimator:
             }
         )
 
-        # logs
-        if self.tensor_image.dtype == tf.quint8:
-            logger.info('quantization mode enabled.')
-
     def __del__(self):
         # self.persistent_sess.close()
         pass
-
-    def get_flops(self):
-        flops = tf.profiler.profile(self.graph, options=tf.profiler.ProfileOptionBuilder.float_operation())
-        return flops.total_float_ops
 
     @staticmethod
     def _quantize_img(npimg):
@@ -560,7 +375,7 @@ class TfPoseEstimator:
         self.heatMat = heatMat_up[0]
         self.pafMat = pafMat_up[0]
         logger.debug('inference- heatMat=%dx%d pafMat=%dx%d' % (
-            self.heatMat.shape[1], self.heatMat.shape[0], self.pafMat.shape[1], self.pafMat.shape[0]))
+        self.heatMat.shape[1], self.heatMat.shape[0], self.pafMat.shape[1], self.pafMat.shape[0]))
 
         t = time.time()
         humans = PoseEstimator.estimate_paf(peaks, self.heatMat, self.pafMat)
