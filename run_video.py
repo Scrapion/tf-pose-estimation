45952a1728fde02d05cf9294b3e15da2e7709825
Scrapion <40768074+Scrapion@users.noreply.github.com>
2020-11-17 21:36:53 +0100

    Update run_video.py

--- run_video.py
+++ run_video.py
@@ -1,21 +1,12 @@
 import argparse
 import logging
 import time
-from google.colab.patches import cv2_imshow
+
 import cv2
 import numpy as np
 
-from IPython.display import clear_output, display
-from PIL import Image
-import base64
-
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
-
-def arrayShow (imageArray):
-    ret, png = cv2.imencode('.png', imageArray)
-    encoded = base64.b64encode(png)
-    return Image(data=encoded.decode('ascii'))
 
 logger = logging.getLogger('TfPoseEstimator-Video')
 logger.setLevel(logging.DEBUG)
@@ -31,38 +22,43 @@ fps_time = 0
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='tf-pose-estimation Video')
     parser.add_argument('--video', type=str, default='')
-    parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')
-    parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
+    parser.add_argument('--resize', type=str, default='0x0',
+                        help='if provided, resize images before they are processed. default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
+    parser.add_argument('--resize-out-ratio', type=float, default=4.0,
+                        help='if provided, resize heatmaps before they are post-processed. default=1.0')
+    parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin')
     parser.add_argument('--show-process', type=bool, default=False,
                         help='for debug purpose, if enabled, speed for inference is dropped.')
     parser.add_argument('--showBG', type=bool, default=True, help='False to show skeleton only.')
     args = parser.parse_args()
 
     logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
-    w, h = model_wh(args.resolution)
-    e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+    
+    w, h = model_wh(args.resize)
+    if w > 0 and h > 0:
+        e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+    else:
+        e = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368))
+            
     cap = cv2.VideoCapture(args.video)
-    f_index=0
+
     if cap.isOpened() is False:
         print("Error opening video stream or file")
     while cap.isOpened():
         ret_val, image = cap.read()
-
-        humans = e.inference(image)
+        
+        logger.debug('image process+')
+        humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
         if not args.showBG:
             image = np.zeros(image.shape)
+        
+        logger.debug('postprocess+')
         image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
 
+        logger.debug('show+')
         cv2.putText(image, "FPS: %f" % (1.0 / (time.time() - fps_time)), (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.imwrite('frame.jpg', image)
-        print('Frame index: ',f_index+1)
-        #cv2_imshow(image)
-        f_index+=1
+        cv2.imshow('tf-pose-estimation result', image)
         fps_time = time.time()
-
-        img_frame=cv2.imread('/content/tf-pose-estimation/frame.jpg')
-        cv2_imshow(img_frame)
-        
         if cv2.waitKey(1) == 27:
             break
 
